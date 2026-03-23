5bf433cb38157651bde4fa77db331cf645b62366
jcSongCSUMB <112451367+jcSongCSUMB@users.noreply.github.com>
2026-03-22 22:18:13 -0700

    Update Score System
    Added the score system with a UI display
    
    Replaced the Game Over text with the final score text

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,14 +1,22 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameOverHandler : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private GameObject AsteroidSpawner;
+    [SerializeField] private TMP_Text gameOverText;
+    [SerializeField] private ScoreSystem scoreSystem;
     
     public void EndGame()
     {
         AsteroidSpawner.gameObject.SetActive(false);
+
+        int finalScore = scoreSystem.EndTimer();
+        gameOverText.text = $"Your score is {finalScore}";
+        
         gameOverDisplay.gameObject.SetActive(true);
     }
     
